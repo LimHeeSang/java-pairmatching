ed1e16431cf712444ccc831bf9230910ad439a91
limheesang <dlagmltkd12@naver.com>
2022-12-05 22:21:43 +0900

    feat: 크루수가 홀수일 경우 페어 매치

--- src/main/java/pairmatching/domain/service/PairMatchMachine.java
+++ src/main/java/pairmatching/domain/service/PairMatchMachine.java
@@ -9,8 +9,22 @@ public class PairMatchMachine {
 
     public static List<Pair> match(List<String> names) {
         List<Pair> pairs = new ArrayList<>();
-        for (int i = 0; i < names.size() - 1; i += 2) {
-            pairs.add(new Pair(names.get(i), names.get(i + 1)));
+
+        if (names.size() % 2 == 0) {
+            for (int i = 0; i < names.size(); i += 2) {
+                pairs.add(new Pair(names.get(i), names.get(i + 1)));
+            }
+        }
+        //0, 1, 2, 3, 4, 5, 6
+        if (names.size() % 2 != 0) {
+            for (int i = 0; i < names.size(); i += 2) {
+                if (i == (names.size() - 1)) {
+                    pairs.add(new Pair(names.get(i), names.get(i - 1)));
+                    pairs.add(new Pair(names.get(i), names.get(i - 2)));
+                } else {
+                    pairs.add(new Pair(names.get(i), names.get(i + 1)));
+                }
+            }
         }
         return pairs;
     }
--- src/test/java/pairmatching/domain/service/PairMatchMachineTest.java
+++ src/test/java/pairmatching/domain/service/PairMatchMachineTest.java
@@ -11,13 +11,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 class PairMatchMachineTest {
 
     @Test
-    void 코스별로_페어매치() {
+    void 페어매치() {
         List<String> names = Arrays.asList("pobi", "jason", "nana", "hanbi", "jundle", "kaka");
         List<Pair> pairs = PairMatchMachine.match(names);
+
         assertThat(pairs.size()).isEqualTo(3);
         assertThat(pairs).containsExactly(
                 new Pair("jason", "pobi"),
                 new Pair("nana", "hanbi"),
                 new Pair("kaka", "jundle"));
+    }
+
+    @Test
+    void 크루수가_홀수일겨우_페어매치() {
+        List<String> names = Arrays.asList("pobi", "jason", "nana", "hanbi", "jundle", "kaka", "heesang");
+        List<Pair> pairs = PairMatchMachine.match(names);
+
+        assertThat(pairs.size()).isEqualTo(5);
+        assertThat(pairs).containsExactly(
+                new Pair("jason", "pobi"),
+                new Pair("nana", "hanbi"),
+                new Pair("jundle", "kaka"),
+                new Pair("heesang", "kaka"),
+                new Pair("jundle", "heesang")
+                );
     }
 }
