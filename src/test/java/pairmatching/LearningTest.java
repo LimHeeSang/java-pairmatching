5ba8c160c9091a510705fa94c017917ed80e9398
limheesang <dlagmltkd12@naver.com>
2022-12-05 17:52:16 +0900

    test: 파일에서 백엔드 크루이름 불러오기

--- src/test/java/pairmatching/LearningTest.java
+++ src/test/java/pairmatching/LearningTest.java
@@ -17,4 +17,14 @@ public class LearningTest {
             System.out.println(str);
         }
     }
+
+    @Test
+    void 파일에서_백엔드_크루이름_불러오기() throws IOException {
+        Scanner scanner = new Scanner(new File("./src/main/resources/backend-crew.md"));
+
+        while (scanner.hasNext()) {
+            String str = scanner.next();
+            System.out.println(str);
+        }
+    }
 }
