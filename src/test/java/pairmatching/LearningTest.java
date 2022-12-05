dcbbac67eed47ce546846f0521108af4aecd7a85
limheesang <dlagmltkd12@naver.com>
2022-12-05 17:51:53 +0900

    test: 파일에서 프론트엔드 크루이름 불러오기

---
+++ src/test/java/pairmatching/LearningTest.java
@@ -0,0 +1,20 @@
+package pairmatching;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class LearningTest {
+
+    @Test
+    void 파일에서_프론트엔드_크루이름_불러오기() throws IOException {
+        Scanner scanner = new Scanner(new File("./src/main/resources/frontend-crew.md"));
+
+        while (scanner.hasNext()) {
+            String str = scanner.next();
+            System.out.println(str);
+        }
+    }
+}
