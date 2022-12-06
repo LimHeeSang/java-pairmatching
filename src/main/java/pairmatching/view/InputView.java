2567e6b9c77a71599c0578a6eedb657d1b2a7e0e
limheesang <dlagmltkd12@naver.com>
2022-12-07 04:28:19 +0900

    refactor: InputView 상수화

--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -7,12 +7,13 @@ import pairmatching.domain.model.Mission;
 
 public class InputView {
 
-    private static final String INPUT_FEATURE_MESSAGE = "기능을 선택하세요.\n" +
-            "1. 페어 매칭\n" +
-            "2. 페어 조회\n" +
-            "3. 페어 초기화\n" +
-            "Q. 종료";
+    private static final String INPUT_FEATURE_MESSAGE = "기능을 선택하세요.\n" + "1. 페어 매칭\n" +
+            "2. 페어 조회\n" + "3. 페어 초기화\n" + "Q. 종료";
     private static final String REGEX_COURSE_LEVEL_MISSION = ", ";
+    private static final String REMATCH_INPUT_MESSAGE = "매칭 정보가 있습니다. 다시 매칭하시겠습니까?\n" + "네 | 아니오";
+    private static final int SPLIT_INDEX_ZERO = 0;
+    private static final int SPLIT_INDEX_ONE = 1;
+    private static final int SPLIT_INDEX_TWO = 2;
 
     private InputView() {
     }
@@ -30,14 +31,13 @@ public class InputView {
     private static CourseLevelMissionDto createCourseLevelMissionDto(String input) {
         String[] splitInput = input.split(REGEX_COURSE_LEVEL_MISSION);
         return new CourseLevelMissionDto(
-                Course.from(splitInput[0]),
-                Level.from(splitInput[1]),
-                Mission.from(splitInput[2]));
+                Course.from(splitInput[SPLIT_INDEX_ZERO]),
+                Level.from(splitInput[SPLIT_INDEX_ONE]),
+                Mission.from(splitInput[SPLIT_INDEX_TWO]));
     }
 
     public static ReMatchType inputRematch() {
-        System.out.println("매칭 정보가 있습니다. 다시 매칭하시겠습니까?\n" +
-                "네 | 아니오");
+        System.out.println(REMATCH_INPUT_MESSAGE);
         return ReMatchType.from(Console.readLine());
     }
 }
