9d61bc3544b0787e582bf26731f68b99dc993b35
limheesang <dlagmltkd12@naver.com>
2022-12-07 19:28:08 +0900

    refactor: Controller 메소드 분리

--- src/main/java/pairmatching/controller/PairMatchingController.java
+++ src/main/java/pairmatching/controller/PairMatchingController.java
@@ -1,5 +1,8 @@
 package pairmatching.controller;
 
+import pairmatching.domain.model.Course;
+import pairmatching.domain.model.Level;
+import pairmatching.domain.model.Mission;
 import pairmatching.domain.service.PairMatchingService;
 import pairmatching.view.CourseLevelMissionDto;
 import pairmatching.view.FeatureType;
@@ -16,37 +19,52 @@ public class PairMatchingController {
     }
 
     public void run() {
-
-        while (true) {
-            FeatureType feature = InputView.inputFeature();
+        FeatureType feature;
+        do {
+            feature = InputView.inputFeature();
 
             if (feature.isPairMatch()) {
-                CourseLevelMissionDto dto = InputView.inputCourseLevelMission();
-
-                if (pairMatchingService.existMatchingResult(dto.getCourse(), dto.getLevel(), dto.getMission())) {
-                    ReMatchType rematch = InputView.inputRematch();
-
-                    if (rematch.isNo()) {
-                        continue;
-                    }
-                }
-                pairMatchingService.pairMatch(dto.getCourse(), dto.getLevel(), dto.getMission());
-                OutputView.printPairMatchingResult(pairMatchingService.getPair(dto.getCourse(), dto.getLevel(), dto.getMission()));
+                operatePairMatch();
             }
-
             if (feature.isPairGet()) {
-                CourseLevelMissionDto dto = InputView.inputCourseLevelMission();
-                OutputView.printPairMatchingResult(pairMatchingService.getPair(dto.getCourse(), dto.getLevel(), dto.getMission()));
+                operatePairGet();
             }
-
             if (feature.isPairInit()) {
-                pairMatchingService.initPair();
+                operatePairInit();
             }
+        } while (!feature.isQuit());
+    }
 
-            if (feature.isQuit()) {
-                break;
-            }
+    private void operatePairMatch() {
+        CourseLevelMissionDto dto = InputView.inputCourseLevelMission();
+        if (pairMatchingService.existMatchingResult(dto.getCourse(), dto.getLevel(), dto.getMission())) {
+            checkRematch(dto);
+            return;
         }
+        pairMatch(dto);
+    }
 
+    private void checkRematch(CourseLevelMissionDto dto) {
+        ReMatchType rematch = InputView.inputRematch();
+        if (rematch.isYes()) {
+            pairMatch(dto);
+        }
+    }
+
+    private void pairMatch(CourseLevelMissionDto dto) {
+        Course course = dto.getCourse();
+        Level level = dto.getLevel();
+        Mission mission = dto.getMission();
+        pairMatchingService.pairMatch(course, level, mission);
+        OutputView.printPairMatchingResult(pairMatchingService.getPair(course, level, mission));
+    }
+
+    private void operatePairGet() {
+        CourseLevelMissionDto dto = InputView.inputCourseLevelMission();
+        OutputView.printPairMatchingResult(pairMatchingService.getPair(dto.getCourse(), dto.getLevel(), dto.getMission()));
+    }
+
+    private void operatePairInit() {
+        pairMatchingService.initPair();
     }
 }
